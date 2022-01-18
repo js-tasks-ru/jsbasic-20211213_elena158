7aee06c00fa53626e26cb66406ebf7ffd8cbaf21
ElenaMorozova88 <morozovaev88@gmail.com>
2022-01-18 20:54:56 +0300

    6-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let result = document.createElement('ul')
+  let HTMLtext = ''
+  for (let i=0;i< friends.length;i++){
+    HTMLtext = HTMLtext + '<li>'+friends[i].firstName+' '+friends[i].lastName+'</li>'
+  }
+  result.innerHTML = HTMLtext;
+  
+  console.log(result)
+  return result;
 }
