6c0847d954740c64e3c7f5f55ded4410950c95be
ElenaMorozova88 <morozovaev88@gmail.com>
2022-01-18 22:12:54 +0300

    5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  var arrBTN = document.getElementsByClassName("toggle-text-button");
+  for (var i = 0; i < arrBTN.length; i++) {
+    arrBTN[i].addEventListener('click', function (){
+      if (text.hidden) {
+        text.hidden = false;
+      } else 
+       {text.hidden = true}
+
+    }, false);
 }
+}
+
